e33c49035dfef973b300a62fbf296825df0221b3
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-01-22 19:28:28 +0100

    Add project files.

---
+++ Here/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Here.MvcApplication" Language="C#" %>
---
+++ Here/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Here.Startup))]
+namespace Here
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
