071009d7430a119fe75b60fe86ce217c6cfd3c33
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-02-20 21:27:51 +0100

    Pure view model  przy dodawaniu/edycji filmu

--- Here/Controllers/MoviesController.cs
+++ Here/Controllers/MoviesController.cs
@@ -47,7 +47,6 @@ namespace Vidly.Controllers
             var genres = _context.Genres.ToList();
             var viewModel = new MovieFormViewModel
             {
-                Movie = new Movie(),
                 Genres = genres
             };
 
@@ -60,9 +59,8 @@ namespace Vidly.Controllers
         {
             if (!ModelState.IsValid)
             {
-                var viewModel = new MovieFormViewModel
+                var viewModel = new MovieFormViewModel(movie)
                 {
-                    Movie = movie,
                     Genres = _context.Genres.ToList()
 
                 };
@@ -98,9 +96,8 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             }
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
--- Here/ViewModels/MovieFormViewModel.cs
+++ Here/ViewModels/MovieFormViewModel.cs
@@ -3,22 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Here.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Here.ViewModels
 {
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> Genres { get; set; }
-        public Movie Movie { get; set; }
+
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte? GenreId { get; set; }
+
+
+        [Required]
+        [Display(Name = "Added date")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Required]
+        [Display(Name = "Release date")]
+        public DateTime? ReleaseDate { get; set; }
+
+
+        [Display(Name = "Number in stock")]
+        [Range(1, 30)]
+        public byte? NumberInStock { get; set; }
+
         public string Title
         {
             get
             {
-                if (Movie != null && Movie.Id != 0)
-                    return "Edit Movie";
-
-                return "New Movie";
+                return Id != 0 ? "Edit Movie" : "New Movie";
             }
+        }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
+
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            CreatedAt = movie.CreatedAt;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
         }
     }
 }
