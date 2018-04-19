b4800049976df9a5d6788eb82f80910bb3344b9c
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-04-19 22:02:30 +0200

    Dokończenie RESTful API dla klientów i filmów

--- Here/App_Start/MappingProfile.cs
+++ Here/App_Start/MappingProfile.cs
@@ -1,10 +1,7 @@
 ﻿using AutoMapper;
 using Here.Dtos;
 using Here.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
 
 namespace Here.App_Start
 {
@@ -13,8 +10,13 @@ namespace Here.App_Start
 
         public MappingProfile()
         {
+            //From Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            //From Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(c => c.Id, opt => opt.Ignore());
         }
 
     }
