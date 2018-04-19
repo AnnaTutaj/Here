c7cc9d3fa8463946d9d49d827c19298e635c0e46
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-04-19 19:01:29 +0200

    AutoMapper, zmiana z PascalCase na camelCase w WebApiConfig (żeby w Java Scripcie ładnie wyglądała notacja z camel case)

---
+++ Here/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Here.Dtos;
+using Here.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Here.App_Start
+{
+    public class MappingProfile : Profile
+    {
+
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+
+    }
+}
