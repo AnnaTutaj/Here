390779dfa5ccf7023ff7ec839106f772720d75c7
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-06-30 11:17:38 +0200

    Ograniczenie dostepu do widokow
    Strona domowa jest dostepna dla niezalogowanych uzytkownikow, reszta wymaga zalogowania sie

--- Here/App_Start/FilterConfig.cs
+++ Here/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Here
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
