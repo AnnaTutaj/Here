a0c01482f253bae4005cd9cde587549f38ed4955
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-07-11 20:48:19 +0200

    Autoryzacja przy zarzadzaniu filmami

--- Here/Controllers/Api/MoviesController.cs
+++ Here/Controllers/Api/MoviesController.cs
@@ -46,6 +46,7 @@ namespace Here.Controllers.Api
 
         // POST /api/movies
         [HttpPost]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult CreateMovie(MovieDto movieDto)
         {
             if (!ModelState.IsValid)
@@ -63,6 +64,7 @@ namespace Here.Controllers.Api
 
         // PUT /api/movie/1
         [HttpPut]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
         {
             if (!ModelState.IsValid)
@@ -85,6 +87,7 @@ namespace Here.Controllers.Api
 
         // DELETE /api/movie/1
         [HttpDelete]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public IHttpActionResult DeleteMovie(int id)
         {
             var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
